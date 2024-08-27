491fea5114dd42ee86a270e087c85806dcabe146
Samkipz <ndiemasam@gmail.com>
2024-08-27 13:34:41 +0300

    removed bugs during deployment 8

--- vsacco/app/profile/page.jsx
+++ vsacco/app/profile/page.jsx
@@ -24,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import prisma from "../lib/prisma";
 // import {Input, Label, Button } from "@/components/ui/input";
 
 const Profile = async () => {
